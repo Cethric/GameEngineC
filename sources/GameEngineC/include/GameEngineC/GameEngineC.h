e40ac90b233e97044370513bdea8c58959082a7a
Cethric <cethric@users.noreply.github.com>
2016-07-24 11:28:53 +1000

    Link all libraries

--- sources/GameEngineC/include/GameEngineC/GameEngineC.h
+++ sources/GameEngineC/include/GameEngineC/GameEngineC.h
@@ -2,16 +2,25 @@
 // Created by Blake Rogan on 21/07/2016.
 //
 #include <GLFW/glfw3.h>
-//#include <AntTweakBar.h>
+#include <AntTweakBar.h>
+#include <cstdio>
+#include <OpenGL/gl.h>
+#include <string>
+#include <strings.h>
+#include <Python.h>
 
 #ifndef PROJECT_GAMEENGINEC_H
 #define PROJECT_GAMEENGINEC_H
+
+using namespace std;
 
 class GameEngineC {
 private:
     typedef void (* callback) (void);
     GLFWwindow *glfWwindow;
-    bool createWindow(int width, int height);
+    TwBar *barOne;
+    float float_var = 0.0f;
+    std::string string_var = "hello world";
 public:
     bool prepare(int width, int height, void (* callback) (void));
     void gameLoop();
--- sources/GameEngineC/source/GameEngineC.cpp
+++ sources/GameEngineC/source/GameEngineC.cpp
@@ -1,22 +1,39 @@
 //
 // Created by Blake Rogan on 21/07/2016.
 //
-#include <cstdio>
-#include <OpenGL/gl.h>
 #include "../include/GameEngineC/GameEngineC.h"
 
+bool loadGLFW(void) {
+    return false;
+}
+
+bool loadPython(void) {
+    return false;
+}
+
+bool loadAntTweakBar(void) {
+    return false;
+}
 
 bool GameEngineC::prepare(int width, int height, void (* callback) (void)) {
     if (!glfwInit()) {
         return false;
     }
-//    TwInit(TW_OPENGL, NULL);
-//    TwWindowSize(width, height);
+    TwInit(TW_OPENGL, NULL);
+    Py_SetProgramName((wchar_t *) "GameEngineC Python");
+    fprintf(stdout, (const char *) Py_GetProgramName());
+    Py_Initialize();
+    PyObject *pName, *pModule, *pDict, *pFunct;
+    PyObject *pArgs, *pValue;
+    pName = PyUnicode_DecodeFSDefault("this will fail");
+    pModule = PyImport_Import(pName);
+    PyErr_Print();
+    PyRun_SimpleString("print('hello world from python');print(__name__)\n");
+    TwWindowSize(width, height);
     callback();
-    return createWindow(width, height);
-}
-
-bool GameEngineC::createWindow(int width, int height) {
+    barOne = TwNewBar("This is a Test Bar");
+    TwAddVarRW(barOne, "Test", TW_TYPE_FLOAT, &float_var, "");
+    TwAddVarRW(barOne, "string_var", TW_TYPE_STDSTRING, &string_var, "");
     glfWwindow = glfwCreateWindow(width, height, "Test", NULL, NULL);
     if (glfWwindow) {
         glfwShowWindow(glfWwindow);
@@ -26,14 +43,52 @@ bool GameEngineC::createWindow(int width, int height) {
     return false;
 }
 
+
+void mouseButtonCallback(GLFWwindow *window, int button, int action, int mods) {
+    int tweak = TwEventMouseButtonGLFW(button, action);
+}
+
+void cursorPosCallback(GLFWwindow *window, double x, double y) {
+    int tweak = TwEventMousePosGLFW((int) x, (int) y);
+}
+
+void scrollCallback(GLFWwindow *window, double x, double y) {
+    int tweak = TwEventMouseWheelGLFW((int) y);
+}
+
+void keyCallback(GLFWwindow *window, int key, int scancode, int action, int mod) {
+    int tweak = TwEventKeyGLFW(key, action);
+}
+
+void charCallback(GLFWwindow *window, unsigned int code_point) {
+    int tweak = TwEventCharGLFW(code_point, GLFW_PRESS);
+}
+
+void windowResize(GLFWwindow *window, int width, int height) {
+    int tweak = TwWindowSize(width, height);
+}
+
+
 void GameEngineC::gameLoop() {
     glfwMakeContextCurrent(glfWwindow);
+
+    glfwSetMouseButtonCallback(glfWwindow, mouseButtonCallback);
+    glfwSetCursorPosCallback(glfWwindow, (GLFWcursorposfun) cursorPosCallback);
+    glfwSetScrollCallback(glfWwindow, (GLFWscrollfun) scrollCallback);
+    glfwSetKeyCallback(glfWwindow, (GLFWkeyfun) keyCallback);
+    glfwSetCharCallback(glfWwindow, (GLFWcharfun) charCallback);
+    glfwSetWindowSizeCallback(glfWwindow, (GLFWwindowsizefun) windowResize);
+
     while (!glfwWindowShouldClose(glfWwindow)) {
         glClear(GL_COLOR_BUFFER_BIT);
         glClearColor(0, 1, 0.5, 1);
+        TwDraw();
+
         glfwSwapBuffers(glfWwindow);
         glfwPollEvents();
     }
 
+    TwTerminate();
+    Py_Finalize();
     glfwTerminate();
 }
